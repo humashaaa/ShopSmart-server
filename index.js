9aaef22940535812d05b64f220dc202748b151c8
Afrida Khan Humashaa <155291628+humashaaa@users.noreply.github.com>
2024-08-17 16:02:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://assignment-12-ef2db.web.app"],
+    origin: ["http://localhost:5173", ],
     credentials: true,
   })
 );
